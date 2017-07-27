33944dd82535c514c33171b153bb381ac94d62a4
saiprakash25 <saiprakash.rao@gmail.com>
2017-07-27 19:18:58 +0530

    Add files via upload

---
+++ weight-converter.js
@@ -0,0 +1,9 @@
+function kgconverter(){
+document.converter.gram.value = document.converter.kg.value * 1000
+
+}
+function gramconverter(){
+document.converter.kg.value = document.converter.gram.value * 0.001
+}
+
+

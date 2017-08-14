ab1a5dc86a3e10c89468183e98ec9bab4162a181
saiprakash25 <saiprakash.rao@gmail.com>
2017-08-14 16:12:36 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,29 @@
+ var html=[
+            {
+                "type":"p",
+                "html":"You must login"
+            },
+            {
+                "name":"username",
+                "id":"txt-username",
+                "caption":"Username",
+                "type":"text",
+                "placeholder":"E.g. u...@example.com"
+            },
+            {
+              "name":"demo",
+                "id":"txt-username",
+                "caption":"demo",
+                "type":"number",
+                "placeholder":"E.g. u..@demo.com"  
+            },
+            {
+                "name":"password",
+                "caption":"Password",
+                "type":"password"
+            },
+            {
+                "type":"submit",
+                "value":"Login"
+            }
+     ];

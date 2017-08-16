cc8273b523b86927fa67e92e177b98ee444c1b57
saiprakash25 <saiprakash.rao@gmail.com>
2017-08-16 11:08:18 +0530

    Add files via upload

---
+++ jsonform.js
@@ -0,0 +1,30 @@
+var z= [
+	{
+		"type": "text",
+		"label": "Text Field",
+		"className": "form-control",
+		"name": "text-1502861315886",
+		"subtype": "text"
+	},
+	{
+		"type": "text",
+		"label": "Text Field",
+		"className": "form-control",
+		"name": "text-1502861316712",
+		"subtype": "text"
+	},
+	{
+		"type": "text",
+		"label": "Text Field",
+		"className": "form-control",
+		"name": "text-1502861317412",
+		"subtype": "text"
+	},
+	{
+		"type": "text",
+		"label": "Text Field",
+		"className": "form-control",
+		"name": "text-1502861318132",
+		"subtype": "text"
+	}
+]
